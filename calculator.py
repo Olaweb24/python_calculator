0b9b967101d61b08a1956361ed5ba8a96c6a33a3
Akinkuolie Olaitan <olaitanclems@gmail.com>
2025-08-03 23:45:00 +0100

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,21 @@
+# Basic Calculator Program
+
+# Get user input
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+# Perform calculation based on the operation
+if operation == '+':
+    print(num1, "+", num2, "=", num1 + num2)
+elif operation == '-':
+    print(num1, "-", num2, "=", num1 - num2)
+elif operation == '*':
+    print(num1, "*", num2, "=", num1 * num2)
+elif operation == '/':
+    if num2 != 0:
+        print(num1, "/", num2, "=", num1 / num2)
+    else:
+        print("Error: Cannot divide by zero.")
+else:
+    print("Invalid operation.")
